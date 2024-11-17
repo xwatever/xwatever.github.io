2695f2c1f74d66af6382740af97f9843d5923170
xwatever <muhammaddarmawan1999@gmail.com>
2024-11-17 21:45:53 +0700

    gh page config modified (9)

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,29 +1,28 @@
 /* eslint-disable no-console */
 
-import execa from "execa";
-import promisify from "promisify";
-import rimraf from "rimraf";
+const execa = require("execa");
+const rmrf = require("rmrf");
+const fs = require("fs");
 
-const rmrf = promisify(rimraf);
-let exitCode = 0;
-try {
-  await execa("git", ["checkout", "--orphan", "gh-pages"]);
-  console.log("Building...");
-  await execa("yarn", ["run", "build-modern"]);
-  // Understand if it's dist or build folder
-  const folderName = existsSync("dist") ? "dist" : "build";
-  await execa("git", ["--work-tree", folderName, "add", "--all"]);
-  await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-  console.log("Pushing to gh-pages...");
-  await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-  await rmrf(folderName, { glob: false });
-  console.log("Successfully deployed");
-} catch (e) {
-  console.log(e.message);
-  exitCode = 1;
-} finally {
-  await promises.writeFile(configFilePath, originPublicPath, fileOpts);
-  await execa("git", ["checkout", "-f", "master"]);
-  await execa("git", ["branch", "-D", "gh-pages"]);
-}
-process.exit(exitCode);
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building started...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await rmrf(folderName, { glob: false });
+    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed, check your settings");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
